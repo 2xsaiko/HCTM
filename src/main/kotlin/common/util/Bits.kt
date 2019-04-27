13f34de2576c60d7df0a72285d5abfa8a829d989
therealfarfetchd <git@therealfarfetchd.dev>
2019-04-27 16:18:34 +0200

    lol

--- src/main/kotlin/common/util/Bits.kt
+++ src/main/kotlin/common/util/Bits.kt
@@ -1,3 +1,8 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package therealfarfetchd.retrocomputers.common.util
 
 import kotlin.experimental.and
